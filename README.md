8182f566419daca54d7545a9c02e68db4ec4f421
Luke Pommersheim <lpommers@gmail.com>
2014-04-29 15:20:12 +0200

    Start

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 Google-Maps-Api
 ===============
 
-Just trying to teach myself how to use the Google Maps api w/JS, PHP, and a SQL database 
+Just trying to teach myself how to use the Google Maps api w/JS, PHP, and a SQL database
+
+You can enter an address.
+
+When you click "add to db" - this address is geocoded and added to the database with an optional name.
+
+When you refresh the page,all the addresses ever entered are displayed and their markers are clickable - revealing the corresponding infoWindow
---
+++ db.php
@@ -0,0 +1,61 @@
+<?php
+
+$address = $_POST['address'];
+$lat = $_POST['sentLat'];
+$lng = $_POST["sentLng"];
+$name = $_POST["name"];
+
+function parseToXML($htmlStr)
+{
+  $xmlStr=str_replace('<','&lt;',$htmlStr);
+  $xmlStr=str_replace('>','&gt;',$xmlStr);
+  $xmlStr=str_replace('"','&quot;',$xmlStr);
+  $xmlStr=str_replace("'",'&#39;',$xmlStr);
+  $xmlStr=str_replace("&",'&amp;',$xmlStr);
+  return $xmlStr;
+}
+
+$connection = mysql_connect("localhost", "root", "root");
+if (!$connection) {
+  die('Not connected : ' . mysql_error());
+}
+
+$db_selected = mysql_select_db("map-api", $connection);
+if (!$db_selected) {
+  die ('Can\'t use db : ' . mysql_error());
+}
+
+// Select all the rows in the markers table
+$query = "SELECT * FROM map WHERE 1";
+$result = mysql_query($query);
+if (!$result) {
+  die('Invalid query: ' . mysql_error());
+}
+
+$insert = "INSERT INTO map (id, name, address, lat, lng) VALUES ( '', '$name', '$address', '$lat', '$lng')";
+
+$insertResult = mysql_query($insert);
+if (!$insertResult) {
+  die('Invalid query: ' . mysql_error());
+}
+
+header("Content-type: text/xml");
+
+echo "<markers>";
+
+// Iterate through the rows, printing XML nodes for each
+while ($row = @mysql_fetch_assoc($result)){
+  // ADD TO XML DOCUMENT NODE
+  echo '<marker ';
+  echo 'name="' . parseToXML($row['name']) . '" ';
+  echo 'address="' . parseToXML($row['address']) . '" ';
+  echo 'lat="' . $row['lat'] . '" ';
+  echo 'lng="' . $row['lng'] . '" ';
+  echo '/>';
+}
+
+// End XML file
+echo '</markers>';
+
+
+ ?>
